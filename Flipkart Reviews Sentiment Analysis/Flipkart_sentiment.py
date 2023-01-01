7f8b64303e447c50449a897762390d7776392aa4
Leonardo Martínez <75277501+LEO22555@users.noreply.github.com>
2023-01-01 14:21:43 -0500

    Day 8

---
+++ Flipkart Reviews Sentiment Analysis/Flipkart_sentiment.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+data = pd.read_csv(r"https://raw.githubusercontent.com/amankharwal/Website-data/master/flipkart_reviews.csv")
+print(data.head())
+
+print(data.isnull().sum())
+import nltk
+import re
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
+
+# cleaning and preparing the column containing reviews before heading to sentiment analysis
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+data["Review"] = data["Review"].apply(clean)
+
+ratings = data["Rating"].value_counts()
+numbers = ratings.index
+quantity = ratings.values
+
+# how most of the people rate the products they buy from Flipkart
+import plotly.express as px
+figure = px.pie(data, values=quantity, names=numbers,hole = 0.5)
+figure.show()
+
+# using a word cloud to visualize the most used words in the reviews column
+text = " ".join(i for i in data.Review)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, 
+                      background_color="white").generate(text)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+# Analyzing the sentiments of Flipkart reviews by adding three columns in this dataset as Positive, Negative, and Neutral by calculating the sentiment scores of the reviews
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Review"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Review"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["Review"]]
+data = data[["Review", "Positive", "Negative", "Neutral"]]
+print(data.head())
+
+
+# how most of the reviewers think about the products and services of Flipkart
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😠 ")
+    else:
+        print("Neutral 🙂 ")
+sentiment_score(x, y, z)
+
+# have a look at the total of Positive, Negative, and Neutral sentiment scores to find a conclusion about Flipkart reviews
+print("Positive: ", x)
+print("Negative: ", y)
+print("Neutral: ", z)
+
+print("most people give Neutral reviews, and a small proportion of people give Negative reviews.")
