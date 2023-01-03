739abfdb95def10b4a1c634c93fd1ccce17c45dd
Leonardo Martínez <75277501+LEO22555@users.noreply.github.com>
2023-01-03 18:48:26 -0500

    Day 10

---
+++ Pfizer Vaccine Sentiment Analysis/pfizer.py
@@ -0,0 +1,88 @@
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+data = pd.read_csv(r"C:\Users\USUARIO\Desktop\DS\Pfizer Vaccine Sentiment Analysis\vaccination_tweets.csv")
+print(data.head())
+
+data.isnull().sum()
+data = data.dropna()
+
+print(data.describe())
+
+# cleaning the text column
+import nltk
+import re
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
+
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+data["text"] = data["text"].apply(clean)
+
+# isualize the word cloud of the text column
+text = " ".join(i for i in data.text)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+# word cloud of the hashtags column
+text = " ".join(i for i in data.hashtags)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+data["user_verified"].value_counts()
+
+# adding three more columns in this dataset as Positive, Negative, and Neutral by calculating the sentiment scores of the text column
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["text"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["text"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["text"]]
+data = data[["text", "Positive", "Negative", "Neutral"]]
+print(data.head())
+
+#  calculatING how most of the people felt about the Pfizer vaccine
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😠 ")
+    else:
+        print("Neutral 🙂 ")
+sentiment_score(x, y, z)
+
+#  total of each sentiment score
+print("Positive: ", x)
+print("Negative: ", y)
+print("Neutral: ", z)
+
+print("The total of positive and negative is very less than Neutral, so we can say that the discussion of the Twitter users was about the awareness of the Pfizer vaccine rather than sharing its benefits or drawbacks.")
