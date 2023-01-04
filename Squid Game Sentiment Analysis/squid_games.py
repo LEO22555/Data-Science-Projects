cba08f70c662605c284ae403ce73f3cc2a0cb6d9
Leonardo Martínez <75277501+LEO22555@users.noreply.github.com>
2023-01-03 19:12:32 -0500

    Day 10

---
+++ Squid Game Sentiment Analysis/squid_games.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+data = pd.read_csv("squid_game.csv")
+print(data.head())
+
+data = data.drop(columns="user_location", axis=1)
+print(data.isnull().sum())
+
+data = data.drop(columns="user_description", axis=1)
+data = data.dropna()
+
+
+import nltk
+import re
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
+
+# preparing before any analysis
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+data["text"] = data["text"].apply(clean)
+
+
+# most used words in the Squid Game opinions using a word cloud
+text = " ".join(i for i in data.text)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+# adding three more columns in this dataset as Positive, Negative, and Neutral by calculating the sentiment scores of the text column
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["text"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["text"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["text"]]
+data = data[["text", "Positive", "Negative", "Neutral"]]
+print(data.head())
+
+# how most people think about the Squid Game
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😠 ")
+    else:
+        print("Neutral 🙂 ")
+sentiment_score(x, y, z)
+
+# have a look at the total of each sentiment score before making any conclusion
+print("Positive: ", x)
+print("Negative: ", y)
+print("Neutral: ", z)
+
+print("The total of negatives is much lower than that of Positive, so we can say that most of the opinions on the Squid Game are positive.")
