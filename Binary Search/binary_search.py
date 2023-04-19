25c60f0622281c10cdf214c6023bfd231fd30b22
Leonardo Martinez Barrios <75277501+LEO22555@users.noreply.github.com>
2023-04-19 09:30:30 -0500

    Day 36

---
+++ Binary Search/binary_search.py
@@ -0,0 +1,18 @@
+def search(nums, target):
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+
+print(search(nums, target))
